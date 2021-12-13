bd986cf5196e7049affb8afa6ae6e2d18cb4d12c
Ming-der Wang <mingderwang@gmail.com>
2021-12-13 16:03:08 +0800

    add pagination in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,101 +1,41 @@
-import Head from "next/head";
+import Link from "next/link";
+import Router from "next/router";
+import fetch from "isomorphic-unfetch";
+import React from "React";
+export default class App extends React.Component {
+  static async getInitialProps({ query: { page = 1 } }) {
+    const r = await fetch(
+      `https://chroniclingamerica.loc.gov/search/titles/results/?terms=michigan&format=json&page=${page}`
+    );
+    const d = await r.json();
+    return {
+      items: d.items,
+      page: parseInt(page, 10),
+    };
+  }
 
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div class="alert alert-success">
-        <div class="flex-1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            class="w-6 h-6 mx-2 stroke-current"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
-            ></path>
-          </svg>
-          <label>Lorem ipsum dolor sit amet, consectetur adip!</label>
-        </div>
-      </div>
-      <button class="btn btn-info">info</button> 
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{" "}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{" "}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  render() {
+    return (
+      <div>
+        <button
+          onClick={() => Router.push(`/?page=${this.props.page - 1}`)}
+          disabled={this.props.page <= 1}
         >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  );
+          PREV
+        </button>
+        <button onClick={() => Router.push(`/?page=${this.props.page + 1}`)}>
+          NEXT
+        </button>
+        <Link href="/?page=1">
+          <a>First page</a>
+        </Link>
+        <p>----</p>
+        <ul>
+          {this.props.items.map(({ title, id }) => (
+            <li key={id}>{title}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
